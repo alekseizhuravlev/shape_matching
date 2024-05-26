18fad43ae4acea8cbbef818d459cbdb09f7bc7e0
Aleksei Zhuravlev <s94zalek@uni-bonn.de>
2024-05-26 09:29:51 +0200

    training diffusion on surreal

---
+++ datasets_code/generate_surreal_shapes.py
@@ -0,0 +1,165 @@
+import sys
+
+sys.path.append('/home/s94zalek/shape_matching/datasets_code')
+
+import numpy as np
+# import smplx
+import torch
+
+from tqdm import tqdm
+
+
+from smpl_webuser.serialization import load_model
+import os
+
+
+def generate_surreal(m, pose, beta):
+    """
+    This function generation 1 human using a random pose and shape estimation from surreal
+    """
+    ## Assign gaussian pose
+    m.pose[:] = pose
+    m.betas[:] = beta
+    # m.pose[0:3]=0
+        
+    point_set = m.r.astype(np.float32)
+    
+    #normalize
+    centroid = np.expand_dims(np.mean(point_set[:,0:3], axis = 0), 0) #Useless because dataset has been normalised already
+    point_set[:,0:3] = point_set[:,0:3] - centroid
+
+    return point_set, m.f
+
+
+def get_random(database, poses, betas):
+    beta_id = np.random.randint(np.shape(betas)[0]-1)
+    beta = betas[beta_id]
+    pose_id = np.random.randint(len(poses)-1)
+    pose_ = database[poses[pose_id]]
+    pose_id = np.random.randint(np.shape(pose_)[0])
+    pose = pose_[pose_id]
+    return pose, beta
+
+
+def generate_shapes(n_body_types_male, n_body_types_female, n_poses_straight, n_poses_bent):
+    
+    database = np.load("/home/s94zalek/shape_matching/data/SURREAL_full/smpl_data.npz")
+    
+    # fix numpy random seed
+    np.random.seed(120)
+    
+    # generate random male betas
+    random_beta_indices_male = np.random.choice(len(database['maleshapes']), n_body_types_male, replace=False)
+    random_betas_male = database['maleshapes'][random_beta_indices_male]
+    m_male = load_model("/home/s94zalek/shape_matching/data/SURREAL_full/smpl/models/basicmodel_m_lbs_10_207_0_v1.0.0.pkl")
+    
+    # generate random female betas
+    random_beta_indices_female = np.random.choice(len(database['femaleshapes']), n_body_types_female, replace=False)
+    random_betas_female = database['femaleshapes'][random_beta_indices_female]
+    m_female = load_model("/home/s94zalek/shape_matching/data/SURREAL_full/smpl/models/basicModel_f_lbs_10_207_0_v1.0.0.pkl")
+    
+    
+    # get pose sequences
+    pose_sequences = [i for i in database.keys() if "pose" in i]
+    
+    # gather all poses from sequences into a single list
+    pose_database = []
+    for i in range(len(pose_sequences)):
+        # length of a pose sequence i
+        pose_length = len(database[pose_sequences[i]])
+        
+        # add all the poses in the sequence to the database
+        # i is the sequence index, j is the pose index in the sequence
+        pose_database += [(i, j) for j in range(pose_length)]
+        
+
+    # sanity check
+    num_poses = 0
+    for i in pose_sequences:
+        num_poses = num_poses + np.shape(database[i])[0]
+    assert len(pose_database) == num_poses, f"Number of poses in the database is {len(pose_database)} but should be {num_poses}"    
+    
+        
+    # generate random poses without replacement
+    random_pose_indices = np.random.choice(len(pose_database), n_poses_straight + n_poses_bent, replace=False)
+    random_poses = []
+    
+    # generate straight poses
+    for i in random_pose_indices[:n_poses_straight]:
+        # get the pose sequence using the 1st index
+        pose_sequence = pose_sequences[pose_database[i][0]]
+        
+        # get the pose in the sequence using the 2nd index
+        pose = database[pose_sequence][pose_database[i][1]]
+        pose[0:3]=0 
+        
+        random_poses.append(pose)
+        
+    # generate bent poses
+    for i in random_pose_indices[n_poses_straight:]:
+        # get the pose sequence using the 1st index
+        pose_sequence = pose_sequences[pose_database[i][0]]
+        
+        # get the pose in the sequence using the 2nd index
+        pose = database[pose_sequence][pose_database[i][1]]
+        
+        # bend the pose
+        a = np.random.randn(12)
+        pose[1] = 0
+        pose[2] = 0
+        pose[3] = -1.0 + 0.1*a[0]
+        pose[4] = 0 + 0.1*a[1]
+        pose[5] = 0 + 0.1*a[2]
+        pose[6] = -1.0 + 0.1*a[0]
+        pose[7] = 0 + 0.1*a[3]
+        pose[8] = 0 + 0.1*a[4]
+        pose[9] = 0.9 + 0.1*a[6]
+        pose[0] = - (-0.8 + 0.1*a[0] )
+        pose[18] = 0.2 + 0.1*a[7]
+        pose[43] = 1.5 + 0.1*a[8]
+        pose[40] = -1.5 + 0.1*a[9]
+        pose[44] = -0.15 
+        pose[41] = 0.15
+        pose[48:54] = 0 
+        
+        random_poses.append(pose)
+        
+
+    # generate the data
+    output = {
+        'verts': [],
+        'faces': [],
+        'poses': [],
+        'betas': []
+    }
+    
+    for beta in tqdm(random_betas_male, desc="Generating male shapes"):
+        for pose in random_poses:
+            verts, faces = generate_surreal(m_male, pose, beta)
+            
+            output['verts'].append(verts)
+            output['faces'].append(faces)
+            output['poses'].append(pose)
+            output['betas'].append(beta)
+            
+    for beta in tqdm(random_betas_female, desc="Generating female shapes"):
+        for pose in random_poses:
+            verts, faces = generate_surreal(m_female, pose, beta)
+            
+            output['verts'].append(verts)
+            output['faces'].append(faces)
+            output['poses'].append(pose)
+            output['betas'].append(beta)
+        
+    # stack the arrays
+    output['verts'] = torch.tensor(np.stack(output['verts']).astype(np.float32))
+    output['faces'] = torch.tensor(np.stack(output['faces']).astype(np.int32))
+    output['poses'] = torch.tensor(np.stack(output['poses']).astype(np.float32))
+    output['betas'] = torch.tensor(np.stack(output['betas']).astype(np.float32))
+        
+    return output   
+
+
+if __name__ == '__main__':
+
+    print(generate_shapes(n_body_types=5, n_poses=5, male=True, bent=False))
---
+++ datasets_code/surreal_full.py
@@ -0,0 +1,183 @@
+import os, re
+import numpy as np
+import scipy.io as sio
+from itertools import product
+from glob import glob
+import trimesh
+import shutil
+
+import torch
+from torch.utils.data import Dataset
+from tqdm import tqdm
+
+import sys
+sys.path.append('/home/s94zalek/shape_matching')
+
+from utils.geometry_util import get_operators
+from datasets_code.generate_surreal_shapes import generate_shapes
+
+
+def get_spectral_ops(item, num_evecs, cache_dir=None):
+    if cache_dir is not None and not os.path.isdir(cache_dir):
+        os.makedirs(cache_dir)
+    _, mass, L, evals, evecs, _, _ = get_operators(item['verts'], item.get('faces'),
+                                                   k=num_evecs,
+                                                   cache_dir=cache_dir)
+    evecs_trans = evecs.T * mass[None]
+    item['evecs'] = evecs[:, :num_evecs]
+    item['evecs_trans'] = evecs_trans[:num_evecs]
+    item['evals'] = evals[:num_evecs]
+    item['mass'] = mass
+    item['L'] = L.to_dense()
+
+    return item
+
+
+class SingleSurrealDataset(Dataset):
+    def __init__(self,
+                 n_body_types_male,
+                 n_body_types_female,
+                 n_poses_straight,
+                 n_poses_bent,
+                 num_evecs=200
+                 ):
+
+        self.data_root = '/home/s94zalek/shape_matching/data/SURREAL'
+        self.num_evecs = num_evecs
+        
+        # generate male and female unbent shapes
+        # male_shapes = generate_shapes(n_body_types=n_body_types, n_poses=n_poses, male=True, bent=False)
+        # female_shapes = generate_shapes(n_body_types=n_body_types, n_poses=n_poses, male=False, bent=False)
+        
+        # # concatenate them
+        # self.shapes = {
+        #     'verts': torch.cat([male_shapes['verts'], female_shapes['verts']], axis=0),
+        #     'faces': torch.cat([male_shapes['faces'], female_shapes['faces']], axis=0),
+        #     'poses': torch.cat([male_shapes['poses'], female_shapes['poses']], axis=0),
+        #     'betas': torch.cat([male_shapes['betas'], female_shapes['betas']], axis=0),
+        # }
+        
+        # if include_bent:
+        #     # bent shapes are 1/6 of the total number of shapes, but at least one
+        #     n_body_types_bent = max(n_body_types // 6, 1)
+        #     n_poses_bent = max(n_poses // 6, 1)
+            
+        #     # generate male and female bent shapes
+        #     male_bent = generate_shapes(
+        #         n_body_types=n_body_types_bent, n_poses=n_poses_bent,
+        #         male=True, bent=True)
+        #     female_bent = generate_shapes(
+        #         n_body_types=n_body_types_bent, n_poses=n_poses_bent,
+        #         male=False, bent=True)
+            
+        #     # concatenate them
+        #     self.shapes = {
+        #         'verts': torch.cat([self.shapes['verts'], male_bent['verts'], female_bent['verts']], axis=0),
+        #         'faces': torch.cat([self.shapes['faces'], male_bent['faces'], female_bent['faces']], axis=0),
+        #         'poses': torch.cat([self.shapes['poses'], male_bent['poses'], female_bent['poses']], axis=0),
+        #         'betas': torch.cat([self.shapes['betas'], male_bent['betas'], female_bent['betas']], axis=0),
+        #     }
+        
+        self.shapes = generate_shapes(n_body_types_male, n_body_types_female, n_poses_straight, n_poses_bent)
+            
+        
+        # load template mesh
+        self.template_mesh = trimesh.load('/home/s94zalek/shape_matching/data/SURREAL_full/template/template.ply')
+
+        # sanity check
+        assert len(self.shapes) > 0, f'No shapes found'
+        assert self.template_mesh is not None, f'No template_mesh found'
+        
+        # make the template object
+        self.template = {
+            'name': 'template',
+            'verts': torch.tensor(self.template_mesh.vertices).float(),
+            'faces': torch.tensor(self.template_mesh.faces).long(),
+            'corr': list(range(len(self.template_mesh.vertices))),
+        }
+        self.template = get_spectral_ops(self.template, num_evecs=self.num_evecs)
+     
+    
+    def get_functional_map(self, data_x, data_y):
+
+        # calculate the map
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        
+        C_gt_xy = torch.linalg.lstsq(
+            data_y['evecs'][data_y['corr']].to(device),
+            data_x['evecs'][data_x['corr']].to(device)
+            ).solution.to('cpu')
+        
+        C_gt_yx = torch.linalg.lstsq(
+            data_x['evecs'][data_x['corr']].to(device),
+            data_y['evecs'][data_y['corr']].to(device)
+            ).solution.to('cpu')
+
+        return C_gt_xy, C_gt_yx
+        
+        
+    def __getitem__(self, index):
+        
+        item = dict()
+        item['name'] = str(index)
+        
+        item['verts'] = self.shapes['verts'][index]
+        item['faces'] = self.shapes['faces'][index]
+        item['poses'] = self.shapes['poses'][index]
+        item['betas'] = self.shapes['betas'][index]
+        
+        # get eigenfunctions/eigenvalues
+        item = get_spectral_ops(item, num_evecs=self.num_evecs,) #cache_dir=os.path.join(self.data_root, 'diffusion'))
+        
+        # 1 to 1 correspondence
+        item['corr'] = list(range(len(item['verts'])))        
+        
+        payload =  {
+            'first': self.template,
+            'second': item,
+        }
+        payload['C_gt_xy'], payload['C_gt_yx'] = self.get_functional_map(payload['first'], payload['second'])
+        
+        return payload
+
+
+    def __len__(self):
+        return len(self.shapes['verts'])
+
+
+if __name__ == '__main__':
+    dataset = SingleSurrealDataset(
+        n_body_types_male=50, n_body_types_female=50, n_poses_straight=88, n_poses_bent=12,
+        num_evecs=50
+    )
+    fmap_path = '/home/s94zalek/shape_matching/data/SURREAL_full/functional_maps/train'
+    evals_path = '/home/s94zalek/shape_matching/data/SURREAL_full/eigenvalues/train'
+    evecs_path = '/home/s94zalek/shape_matching/data/SURREAL_full/eigenvectors/train'
+    
+    shutil.rmtree(fmap_path, ignore_errors=True)
+    shutil.rmtree(evals_path, ignore_errors=True)
+    shutil.rmtree(evecs_path, ignore_errors=True)
+    
+    os.makedirs(fmap_path, exist_ok=True)
+    os.makedirs(evals_path, exist_ok=True)
+    os.makedirs(evecs_path, exist_ok=True)
+
+    evals_file =os.path.join(evals_path, 'eigenvalues.txt')
+    fmaps_file =os.path.join(fmap_path, 'functional_maps.txt')
+    
+    for i in tqdm(range(len(dataset))):
+        data = dataset[i]
+        
+        with open(fmaps_file, 'ab') as f:
+            np.savetxt(f, data['C_gt_xy'].numpy().flatten().astype(np.float32), newline=" ")
+            f.write(b'\n')
+            
+        with open(evals_file, 'ab') as f:
+            np.savetxt(f, data['second']['evals'].numpy().astype(np.float32), newline=" ")
+            f.write(b'\n')
+            
+        # with open(os.path.join(evecs_path, f'{int(data["second"]["name"]):05d}.txt'), 'wb') as f:
+        #     np.savetxt(f, data['second']['evecs'].numpy())
+            
+        
+        
